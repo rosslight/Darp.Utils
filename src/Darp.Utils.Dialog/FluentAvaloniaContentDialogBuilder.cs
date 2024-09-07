ee4c1a75f15c0f16a7c7fd73c20f0c427dbdaa89
OleRosskamp <ole.rosskamp@rosslight.de>
2024-09-07 16:47:33 +0200

    fix: Fix not possible isEnabled on SetCloseButton

--- src/Darp.Utils.Dialog/FluentAvaloniaContentDialogBuilder.cs
+++ src/Darp.Utils.Dialog/FluentAvaloniaContentDialogBuilder.cs
@@ -13,69 +13,66 @@ using FluentAvalonia.UI.Controls;
 
 /// <summary> The builder based on the <see cref="ContentDialog"/> of FluentAvalonia </summary>
 /// <typeparam name="TContent"> The type of the content </typeparam>
-public sealed class ContentDialogBuilder<TContent>
+public sealed class FluentAvaloniaContentDialogBuilder<TContent> : IContentDialogBuilder<TContent>
 {
     private readonly AvaloniaDialogService _dialogService;
-    private readonly ContentDialog _dialog;
+    private readonly TopLevel? _topLevel;
+    internal ContentDialog Dialog { get; }
 
     private CancellationTokenSource? _cancelTokenSource;
-    /// <summary> The content of the dialog </summary>
+
+    /// <inheritdoc />
+    public string Title { get; }
+    /// <inheritdoc />
     public TContent Content { get; }
 
-    /// <summary> Initialize a new <see cref="ContentDialogBuilder{TContent}"/> </summary>
+    /// <summary> Initialize a new <see cref="FluentAvaloniaContentDialogBuilder{TContent}"/> </summary>
     /// <param name="dialogService"> The dialogService which created the builder </param>
     /// <param name="title"> The title of the dialog </param>
     /// <param name="content"> The content of the dialog </param>
-    public ContentDialogBuilder(AvaloniaDialogService dialogService, string title, TContent content)
+    /// <param name="topLevel"> An optional TopLevel to show the dialog on </param>
+    public FluentAvaloniaContentDialogBuilder(AvaloniaDialogService dialogService,
+        string title,
+        TContent content,
+        TopLevel? topLevel = null)
     {
         _dialogService = dialogService;
-        _dialog = new ContentDialog
+        _topLevel = topLevel;
+        Dialog = new ContentDialog
         {
             Title = title,
             Content = content,
         };
-        _dialog.Opened += (dialog, _) =>
+        Dialog.Opened += (dialog, _) =>
         {
             IInputElement? firstOrDefault = dialog.GetLogicalDescendants()
                 .OfType<IInputElement>()
                 .FirstOrDefault(x => x.Focusable);
             firstOrDefault?.Focus();
         };
-        _dialog.DataTemplates.Add(new FuncDataTemplate<MessageBoxModel>((model, _) => model.IsSelectable
+        Dialog.DataTemplates.Add(new FuncDataTemplate<MessageBoxModel>((model, _) => model.IsSelectable
             ? new SelectableTextBlock { Text = model.Message, TextWrapping = TextWrapping.Wrap }
             : new TextBlock { Text = model.Message, TextWrapping = TextWrapping.Wrap }));
+        Title = title;
         Content = content;
     }
 
-    /// <summary> Sets the button which is shown as the default button </summary>
-    /// <param name="defaultButton"> The default <see cref="ContentDialogButton"/> </param>
-    /// <returns> The same <see cref="ContentDialogBuilder{TContent}"/> instance </returns>
-    public ContentDialogBuilder<TContent> SetDefaultButton(ContentDialogButton defaultButton)
+    /// <inheritdoc />
+    public IContentDialogBuilder<TContent> SetDefaultButton(ContentDialogButton defaultButton)
     {
-        _dialog.DefaultButton = defaultButton;
+        Dialog.DefaultButton = defaultButton;
         return this;
     }
 
-    /// <summary>
-    /// Enables the <see cref="ContentDialogButton.Close"/> button and sets the text to be displayed.
-    /// </summary>
-    /// <param name="text"> The text to be shown on the button </param>
-    /// <param name="isEnabled"> An observable which is bound to the button and can enable/disable it </param>
-    /// <param name="onClick"> A callback function on button click. Returning 'false' aborts the close operation </param>
-    /// <returns> The same <see cref="ContentDialogBuilder{TContent}"/> instance </returns>
-    public ContentDialogBuilder<TContent> SetCloseButton(string text,
-        IObservable<bool>? isEnabled = null,
+    /// <inheritdoc />
+    public IContentDialogBuilder<TContent> SetCloseButton(string text,
         Func<TContent, bool>? onClick = null)
     {
-        _dialog.CloseButtonText = text;
-        if (isEnabled is not null)
-        {
-            _dialog[!ContentDialog.IsPrimaryButtonEnabledProperty] = isEnabled.ToBinding();
-        }
+        Dialog.CloseButtonText = text;
 
         if (onClick is not null)
         {
-            _dialog.PrimaryButtonClick += (_,args) =>
+            Dialog.CloseButtonClick += (_,args) =>
             {
                 var shouldClose = onClick(Content);
                 args.Cancel = !shouldClose;
@@ -84,22 +81,16 @@ public sealed class ContentDialogBuilder<TContent>
         return this;
     }
 
-    /// <summary>
-    /// Enables the <see cref="ContentDialogButton.Primary"/> button and sets the text to be displayed.
-    /// </summary>
-    /// <param name="text"> The text to be shown on the button </param>
-    /// <param name="isEnabled"> An observable which is bound to the button and can enable/disable it </param>
-    /// <param name="onClick"> A callback function on button click. Returning 'false' aborts the close operation </param>
-    /// <returns> The same <see cref="ContentDialogBuilder{TContent}"/> instance </returns>
-    public ContentDialogBuilder<TContent> SetPrimaryButton(string text,
+    /// <inheritdoc />
+    public IContentDialogBuilder<TContent> SetPrimaryButton(string text,
         IObservable<bool>? isEnabled = null,
         Func<TContent, CancellationToken, Task<bool>>? onClick = null)
     {
-        _dialog.PrimaryButtonText = text;
+        Dialog.PrimaryButtonText = text;
         BehaviorSubject<bool> notExecutingSubject = _dialogService.RegisterDisposable(new BehaviorSubject<bool>(true));
         if (isEnabled is not null)
         {
-            _dialog[!ContentDialog.IsPrimaryButtonEnabledProperty] = isEnabled
+            Dialog[!ContentDialog.IsPrimaryButtonEnabledProperty] = isEnabled
                 .CombineLatest(notExecutingSubject)
                 .Select(x => x.First && x.Second)
                 .ToBinding();
@@ -107,7 +98,7 @@ public sealed class ContentDialogBuilder<TContent>
 
         if (onClick is not null)
         {
-            _dialog.PrimaryButtonClick += async (_,args) =>
+            Dialog.PrimaryButtonClick += async (_,args) =>
             {
                 Deferral? deferral = null;
                 try
@@ -127,22 +118,16 @@ public sealed class ContentDialogBuilder<TContent>
         return this;
     }
 
-    /// <summary>
-    /// Enables the <see cref="ContentDialogButton.Secondary"/> button and sets the text to be displayed.
-    /// </summary>
-    /// <param name="text"> The text to be shown on the button </param>
-    /// <param name="isEnabled"> An observable which is bound to the button and can enable/disable it </param>
-    /// <param name="onClick"> A callback function on button click. Returning 'false' aborts the close operation </param>
-    /// <returns> The same <see cref="ContentDialogBuilder{TContent}"/> instance </returns>
-    public ContentDialogBuilder<TContent> SetSecondaryButton(string text,
+    /// <inheritdoc />
+    public IContentDialogBuilder<TContent> SetSecondaryButton(string text,
         IObservable<bool>? isEnabled = null,
         Func<TContent, CancellationToken, Task<bool>>? onClick = null)
     {
-        _dialog.SecondaryButtonText = text;
+        Dialog.SecondaryButtonText = text;
         BehaviorSubject<bool> notExecutingSubject = _dialogService.RegisterDisposable(new BehaviorSubject<bool>(true));
         if (isEnabled is not null)
         {
-            _dialog[!ContentDialog.IsSecondaryButtonEnabledProperty] = isEnabled
+            Dialog[!ContentDialog.IsSecondaryButtonEnabledProperty] = isEnabled
                 .CombineLatest(notExecutingSubject)
                 .Select(x => x.First && x.Second)
                 .ToBinding();
@@ -150,7 +135,7 @@ public sealed class ContentDialogBuilder<TContent>
 
         if (onClick is not null)
         {
-            _dialog.SecondaryButtonClick += async (_,args) =>
+            Dialog.SecondaryButtonClick += async (_,args) =>
             {
                 Deferral? deferral = null;
                 try
@@ -170,15 +155,13 @@ public sealed class ContentDialogBuilder<TContent>
         return this;
     }
 
-    /// <summary> Show the current dialog </summary>
-    /// <param name="cancellationToken"> The CancellationToken to cancel the operation </param>
-    /// <returns> The task with the <see cref="ContentDialogResult"/> after the dialog has closed </returns>
+    /// <inheritdoc />
     public async Task<ContentDialogResult> ShowAsync(CancellationToken cancellationToken = default)
     {
         try
         {
             _cancelTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-            return await _dialog.ShowAsync().ConfigureAwait(true);
+            return await Dialog.ShowAsync(_topLevel).ConfigureAwait(true);
         }
         finally
         {
