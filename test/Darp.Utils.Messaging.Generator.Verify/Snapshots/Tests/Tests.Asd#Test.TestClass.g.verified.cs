68da6d1c14839ac7be23439d7859011c64db0132
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-03-03 17:41:13 +0100

    test: Add verify testing project for messaging source generator

---
+++ test/Darp.Utils.Messaging.Generator.Verify/Snapshots/Tests/Tests.Asd#Test.TestClass.g.verified.cs
@@ -0,0 +1,43 @@
+﻿//HintName: Test.TestClass.g.cs
+namespace Test
+{
+    partial class TestClass : global::Darp.Utils.Messaging.IMessageSinkProvider
+    {
+        private ___MessageSink? _;
+
+        /// <inheritdoc />
+        public global::Darp.Utils.Messaging.IMessageSink GetMessageSink()
+        {
+            _eventReceiverProxy ??= new ___MessageSink(this);
+            return _eventReceiverProxy;
+        }
+
+        private sealed class ___MessageSink(TestClass parent)
+            : global::Darp.Utils.Messaging.IMessageSink<int>,
+                global::Darp.Utils.Messaging.IMessageSink<ReadOnlySpan<byte>>,
+                global::Darp.Utils.Messaging.IAnyMessageSink
+        {
+            private readonly TestClass _parent = parent;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Publish(in int message)
+            {
+                _parent.OnInt(message);
+                OnIntStatic(message);
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Publish(in global::System.ReadOnlySpan<byte> message)
+            {
+                _parent.OnSpan(message);
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Publish<T>(in T message)
+                where T : allows ref struct
+            {
+                _parent.Any(message);
+            }
+        }
+    }
+}
---
+++ test/Darp.Utils.Messaging.Generator.Verify/Tests.cs
@@ -0,0 +1,32 @@
+﻿namespace Darp.Utils.Messaging.Generator.Verify;
+
+public class Tests
+{
+    [Fact]
+    public async Task Asd()
+    {
+        const string code = """
+            using System;
+            using Darp.Utils.Messaging;
+
+            namespace Test;
+
+            public sealed partial class TestClass
+            {
+                [MessageSink]
+                private void OnInt(int message) { }
+
+                [MessageSink]
+                private static void OnIntStatic(int message) { }
+
+                [MessageSink]
+                private void OnSpan(ReadOnlySpan<byte> message) { }
+
+                [MessageSink]
+                private void Any<T>(T message)
+                    where T : allows ref struct { }
+            }
+            """;
+        await VerifyHelper.VerifyMessagingGenerator(code);
+    }
+}
