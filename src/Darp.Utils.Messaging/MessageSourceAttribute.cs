2072308d7cbd392bfe2e60c1204bbc3034d91736
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-03-05 14:23:44 +0100

    build: Include readme and skip generator project on build

--- src/Darp.Utils.Messaging/MessageSourceAttribute.cs
+++ src/Darp.Utils.Messaging/MessageSourceAttribute.cs
@@ -1,4 +1,5 @@
 namespace Darp.Utils.Messaging;
 
+/// <summary> Marks a message source for the source generator </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
 public sealed class MessageSourceAttribute : Attribute;
