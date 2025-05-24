8b5096b6003982e7d2afce12a21f39c0c4e621ca
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-05-24 15:30:17 +0200

    feat: Add services optionally only

--- src/Darp.Utils.Assets/AssetsServiceCollectionExtensions.cs
+++ src/Darp.Utils.Assets/AssetsServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@ namespace Darp.Utils.Assets;
 
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 /// <summary> Extension class for <see cref="IServiceCollection"/> </summary>
 public static class AssetsServiceCollectionExtensions
@@ -14,11 +15,16 @@ public static class AssetsServiceCollectionExtensions
     /// <param name="folderPath"> The path to the folder </param>
     /// <param name="relativePath"> The relative path inside the AppData </param>
     /// <returns> A reference to this instance after the operation has completed. </returns>
+    /// <remarks> Will only be added if the asset service hasn't already been registered. </remarks>
     public static IServiceCollection AddFolderAssetsService(
         this IServiceCollection serviceCollection,
         string folderPath,
         string relativePath
-    ) => serviceCollection.AddTransient<IFolderAssetsService>(_ => new FolderAssetsService(folderPath, relativePath));
+    )
+    {
+        serviceCollection.TryAddTransient<IFolderAssetsService>(_ => new FolderAssetsService(folderPath, relativePath));
+        return serviceCollection;
+    }
 
     /// <summary>
     /// Adds an <see cref="IAppDataAssetsService"/> to the serviceCollection.
@@ -27,10 +33,15 @@ public static class AssetsServiceCollectionExtensions
     /// <param name="serviceCollection"> The <see cref="IServiceCollection"/> to add the service to.</param>
     /// <param name="relativePath"> The relative path inside the <see cref="Environment.SpecialFolder.ApplicationData"/> </param>
     /// <returns> A reference to this instance after the operation has completed. </returns>
+    /// <remarks> Will only be added if the asset service hasn't already been registered. </remarks>
     public static IServiceCollection AddAppDataAssetsService(
         this IServiceCollection serviceCollection,
         string relativePath
-    ) => serviceCollection.AddTransient<IAppDataAssetsService>(_ => new AppDataAssetsService(relativePath));
+    )
+    {
+        serviceCollection.TryAddTransient<IAppDataAssetsService>(_ => new AppDataAssetsService(relativePath));
+        return serviceCollection;
+    }
 
     /// <summary>
     /// Adds an <see cref="IProgramDataAssetsService"/> to the serviceCollection.
@@ -39,15 +50,21 @@ public static class AssetsServiceCollectionExtensions
     /// <param name="serviceCollection"> The <see cref="IServiceCollection"/> to add the service to.</param>
     /// <param name="relativePath"> The relative path inside the <see cref="Environment.SpecialFolder.CommonApplicationData"/> </param>
     /// <returns> A reference to this instance after the operation has completed. </returns>
+    /// <remarks> Will only be added if the asset service hasn't already been registered. </remarks>
     public static IServiceCollection AddProgramDataAssetsService(
         this IServiceCollection serviceCollection,
         string relativePath
-    ) => serviceCollection.AddTransient<IProgramDataAssetsService>(_ => new ProgramDataAssetsService(relativePath));
+    )
+    {
+        serviceCollection.TryAddTransient<IProgramDataAssetsService>(_ => new ProgramDataAssetsService(relativePath));
+        return serviceCollection;
+    }
 
     /// <summary> Adds an <see cref="IEmbeddedResourceAssetsService"/> to the serviceCollection. </summary>
     /// <param name="serviceCollection"> The <see cref="IServiceCollection"/> to add the service to.</param>
     /// <typeparamref name="TAssemblyMarker"> A type which is contained in the assembly and can be used as a marker </typeparamref>
     /// <returns> A reference to this instance after the operation has completed. </returns>
+    /// <remarks> Will only be added if the asset service hasn't already been registered. </remarks>
     public static IServiceCollection AddEmbeddedResourceAssetsService<TAssemblyMarker>(
         this IServiceCollection serviceCollection
     ) => serviceCollection.AddEmbeddedResourceAssetsService(typeof(TAssemblyMarker).Assembly);
@@ -56,17 +73,25 @@ public static class AssetsServiceCollectionExtensions
     /// <param name="serviceCollection"> The <see cref="IServiceCollection"/> to add the service to.</param>
     /// <param name="assembly"> The assembly which contains the EmbeddedResources </param>
     /// <returns> A reference to this instance after the operation has completed. </returns>
+    /// <remarks> Will only be added if the asset service hasn't already been registered. </remarks>
     public static IServiceCollection AddEmbeddedResourceAssetsService(
         this IServiceCollection serviceCollection,
         Assembly assembly
-    ) =>
-        serviceCollection.AddTransient<IEmbeddedResourceAssetsService>(_ => new EmbeddedResourceAssetsService(
+    )
+    {
+        serviceCollection.TryAddTransient<IEmbeddedResourceAssetsService>(_ => new EmbeddedResourceAssetsService(
             assembly
         ));
+        return serviceCollection;
+    }
 
     /// <summary> Adds an <see cref="IBaseDirectoryAssetsService"/> to the serviceCollection. </summary>
     /// <param name="serviceCollection"> The <see cref="IServiceCollection"/> to add the service to.</param>
     /// <returns> A reference to this instance after the operation has completed. </returns>
-    public static IServiceCollection AddBaseDirectoryAssetsService(this IServiceCollection serviceCollection) =>
-        serviceCollection.AddTransient<IBaseDirectoryAssetsService>(_ => new BaseDirectoryAssetsService());
+    /// <remarks> Will only be added if the asset service hasn't already been registered. </remarks>
+    public static IServiceCollection AddBaseDirectoryAssetsService(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.TryAddTransient<IBaseDirectoryAssetsService>(_ => new BaseDirectoryAssetsService());
+        return serviceCollection;
+    }
 }
--- src/Darp.Utils.Configuration/ConfigurationServiceCollectionExtensions.cs
+++ src/Darp.Utils.Configuration/ConfigurationServiceCollectionExtensions.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization.Metadata;
 using Assets.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 /// <summary> Extension class for <see cref="IServiceCollection"/> </summary>
 public static class ConfigurationServiceCollectionExtensions
@@ -16,20 +17,24 @@ public static class ConfigurationServiceCollectionExtensions
     /// <param name="configFileName"> The name of the config file containing the <typeparamref name="TConfig"/> </param>
     /// <param name="typeInfo">Metadata about the type to convert.</param>
     /// <typeparam name="TConfig"> The type of the config </typeparam>
-    /// <typeparam name="TAssetsService"> The type of the assets service </typeparam>
+    /// <typeparam name="TAssetsService"> The type of the asset service </typeparam>
     /// <returns> A reference to this instance after the operation has completed. </returns>
+    /// <remarks> Will only be added if the configuration service hasn't already been registered. </remarks>
     public static IServiceCollection AddConfigurationFile<TConfig, TAssetsService>(
         this IServiceCollection serviceCollection,
         string configFileName,
         JsonTypeInfo<TConfig> typeInfo
     )
         where TAssetsService : IAssetsService
-        where TConfig : new() =>
-        serviceCollection.AddSingleton<IConfigurationService<TConfig>>(provider => new ConfigurationService<TConfig>(
+        where TConfig : new()
+    {
+        serviceCollection.TryAddSingleton<IConfigurationService<TConfig>>(provider => new ConfigurationService<TConfig>(
             configFileName,
             provider.GetRequiredService<TAssetsService>(),
             typeInfo
         ));
+        return serviceCollection;
+    }
 
     /// <summary>
     /// Adds a new configuration file and registers a corresponding <see cref="IConfigurationService{TConfig}"/>.
@@ -40,6 +45,7 @@ public static class ConfigurationServiceCollectionExtensions
     /// <typeparam name="TConfig"> The type of the config </typeparam>
     /// <typeparam name="TAssetsService"> The type of the assets service </typeparam>
     /// <returns> A reference to this instance after the operation has completed. </returns>
+    /// <remarks> Will only be added if the configuration service hasn't already been registered. </remarks>
     [RequiresUnreferencedCode(
         "JSON serialization and deserialization might require types that cannot be statically analyzed. Use the overload that takes a JsonTypeInfo or JsonSerializerContext, or make sure all of the required types are preserved."
     )]
@@ -51,9 +57,12 @@ public static class ConfigurationServiceCollectionExtensions
         string configFileName
     )
         where TAssetsService : IAssetsService
-        where TConfig : new() =>
-        serviceCollection.AddSingleton<IConfigurationService<TConfig>>(provider => new ConfigurationService<TConfig>(
+        where TConfig : new()
+    {
+        serviceCollection.TryAddSingleton<IConfigurationService<TConfig>>(provider => new ConfigurationService<TConfig>(
             configFileName,
             provider.GetRequiredService<TAssetsService>()
         ));
+        return serviceCollection;
+    }
 }
