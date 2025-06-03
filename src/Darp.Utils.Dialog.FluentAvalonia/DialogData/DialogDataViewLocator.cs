26ac68140ce84704d67d86627f41af1e39c9c0f1
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-06-03 13:27:00 +0200

    refactor: Re-introduce idialogdata check

--- src/Darp.Utils.Dialog.FluentAvalonia/DialogData/DialogDataViewLocator.cs
+++ src/Darp.Utils.Dialog.FluentAvalonia/DialogData/DialogDataViewLocator.cs
@@ -13,10 +13,14 @@ public sealed class DialogDataViewLocator : IDataTemplate
     public Control Build(object? param) =>
         param switch
         {
+            not IDialogData => new TextBlock
+            {
+                Text = "No DialogData provided. This should not happen (because of Match)",
+            },
             InputDialogViewModel vm => new InputDialogView { DataContext = vm },
             MessageBoxViewModel vm => new MessageBoxView { DataContext = vm },
             UsernamePasswordViewModel vm => new UsernamePasswordView { DataContext = vm },
-            _ => new TextBlock { Text = $"DialogData is not registered: {param?.GetType()}" },
+            _ => new TextBlock { Text = $"DialogData is not registered: {param.GetType()}" },
         };
 
     /// <inheritdoc />
