95bd53ae017d98a1cd01bb23cd02896fd50dba6d
OleRoss <ole.rosskamp@rosslight.de>
2026-04-29 19:34:18 +0200

    feat: Remove CodeMirror
    The package did not work properly and was responsible for security vulnerability warnings

--- samples/AvaloniaApp/App.axaml.cs
+++ samples/AvaloniaApp/App.axaml.cs
@@ -1,14 +1,11 @@
 namespace AvaloniaApp;
 
-using System.Reflection;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using Darp.Utils.CodeMirror;
 using Darp.Utils.Dialog;
 using Darp.Utils.Dialog.FluentAvalonia;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using ViewModels;
 using Views;
 
@@ -25,28 +22,9 @@ public sealed partial class App : Application
                 .AddSingleton<IDialogService>(_ => new AvaloniaDialogService())
                 .BuildServiceProvider();
             MainWindowViewModel vm = provider.GetRequiredService<MainWindowViewModel>();
-            _ = vm.CodeMirror.StartBackendAsync(
-                onBuild: builder => builder.Services.AddLogging(loggingBuilder => loggingBuilder.AddConsole()),
-                onConfigureCSharp: options =>
-                    options
-                        .SetScriptMode(true, typeof(Globals))
-                        .SetMetadataReferencesFromAssembly(
-                            Assembly.Load("System.Runtime"),
-                            Assembly.Load("netstandard"),
-                            typeof(Globals).Assembly,
-                            typeof(object).Assembly
-                        )
-                        .AddUsings("System")
-            );
             desktop.MainWindow = new MainWindow { ViewModel = vm };
         }
 
         base.OnFrameworkInitializationCompleted();
     }
-}
-
-public class Globals(string str = "Hello!")
-{
-    public string SomeStaticString { get; } = str;
-    public int myInt = 123;
 }
